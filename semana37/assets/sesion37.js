4d3c9067d780ec763392b0aa0be630805aef32be
Paredes23 <jparedes@estudiantes.crackthecode.la>
2021-12-01 12:36:47 -0500

    Este es el avance de clase

---
+++ semana37/assets/sesion37.js
@@ -0,0 +1,56 @@
+/*
+const listaDeNumeros = [2,3,4,7,11];
+console.log(listaDeNumeros[0]);
+console.log(listaDeNumeros[4]);
+*/
+/*
+let numerosImpares = [1,2,3,5,7];
+cantidad = numerosImpares.length;
+console.log("primera forma:") 
+for (i=0; cantidad>i; i++){
+    console.log(numerosImpares[i])
+}
+console.log("segunda forma:")
+for(const valor of numerosImpares){
+    console.log(valor);
+}
+*/
+
+//Practicando arreglos
+/*
+const listaDeNumeros = [2,3,5,7,11];
+listaDeNumeros[2]= 13;
+console.log(listaDeNumeros[2]);
+*/
+//cambiando valores
+/*
+listaDeNumeros=[];
+listaDeNumeros[0]=100;
+listaDeNumeros[1]=101;
+listaDeNumeros[2]=-10;
+listaDeNumeros[3]=20;
+listaDeNumeros[4]=-5;
+console.log(listaDeNumeros[2])
+
+
+
+const mixedArray = [3, "hola mundo", 10.5];
+cantidad = mixedArray.length;
+for (i=0; cantidad>i; i++){
+console.log(mixedArray[i]);
+}
+*/
+
+let questions = [
+  ['¿Cual es la capital de Perú?' , 'Lima'],
+  ['¿Cuantos colores tiene el arcoiris?', 7 ],
+  ['¿Cúal es la moneda de Perú?', 'Sol'],
+]
+
+console.log(questions[0][0]);
+console.log(questions[0][1]);
+console.log(questions[1][0]);
+
+console.log(questions[1][1]);
+console.log(questions[2][0]);
+console.log(questions[2][1]);
